d9af13b6c8b6d1f5b29c9bf93483f2e67ad97714
ChairmanFed15 <nebulablaze15@gmail.com>
2024-07-30 21:04:23 +1200

    got rid of an import that wasn't being used that I missed

--- src/main/java/net/chairmanfed/neotestmod/block/ModBlock.java
+++ src/main/java/net/chairmanfed/neotestmod/block/ModBlock.java
@@ -7,8 +7,6 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import java.util.function.Supplier;
-
 public class ModBlock {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(NeoTestMod.MODID);
 
